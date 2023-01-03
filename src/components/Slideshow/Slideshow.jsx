b5ca104ad1c71d8997650ed1e9316a74c596069f
Hugo Kai <tank5460@hotmail.fr>
2023-01-03 11:07:50 +0100

    slideshow complete

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -20,16 +20,23 @@ class Slideshow extends React.Component {
         return (
             <div className="carousel">
                 {this.props.pictures.map((picture, index) => {
-                            return (
+                    const currentSlide = this.state.currentSlide;
+                    const length = this.props.pictures.length;
+                        return (
+                            <React.Fragment key={index}>
                                 <img
                                     alt=""
                                     src={picture}
-                                    key={index}
-                                    className={index === this.state.currentSlide ? "carrousel__img" : "hide-img"}></img>
-                            );
-                        })}
-                <img src={arrow}  alt='précédente' onClick={() => {this.prevSlide();}} className='arrow_left'/>
-                <img src={arrow}  alt='précédente' onClick={() => {this.nextSlide();}} className='arrow_right'/>
+                                    className={index === this.state.currentSlide ? "carrousel_img" : "hide-img"}>
+                                </img>
+                            </React.Fragment>
+                        )
+                })}
+                <div className = {this.props.pictures.length === 1 ? "hide-arrow" :""}>
+                    <p className="bullet-point">{this.state.currentSlide+1}/{this.props.pictures.length}</p>
+                    <img src={arrow}  alt='précédente' onClick={() => {this.prevSlide();}} className='arrow_left'/>
+                    <img src={arrow}  alt='suivante' onClick={() => {this.nextSlide();}} className='arrow_right'/>
+                </div>
             </div>
         )
     }
