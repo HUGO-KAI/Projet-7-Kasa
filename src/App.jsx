9b1bc3efddc500c43a049884125a4c27c8675d94
Hugo Kai <tank5460@hotmail.fr>
2023-01-06 18:14:03 +0100

    final version

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,27 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { createGlobalStyle } from 'styled-components'
 import Header from './components/Header/Header'
 import Home from './pages/Home/Home'
 import Logement from './pages/Logement/Logement'
 import About from './pages/About/About'
 import Footer from './components/Footer/Footer'
 import Error from './pages/Error/Error'
+import './App.css'
 
-const GlobalStyle = createGlobalStyle`
-    #root {
-      min-width:375px;
-      max-width:1440px;
-      margin:auto auto;
-      width:100%;
-      font-family: Montserrat, sans-serif;
-      box-sizing: border-box;
-    }
-    a{
-      text-decoration: none;
-    }
-    img{
-      width:100%;
-      object-fit: cover;
-  }
-`
-
+//React Router
 function App() {
   return (
     <div>
-      <GlobalStyle />
-        <Router>
-          <Header />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="logements/:logementId" element={<Logement />} />
-              <Route path="about" element={<About />} />
-              <Route path="*" element={<Error />} />
-            </Routes>
-          <Footer />
-        </Router>
+      <Router>
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="logements/:logementId" element={<Logement />} />
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
